a29fbee35d4a73bd63d679633f000c3f40a42862
Andrea Bonaventura <andreabonaventura802@gmail.com>
2023-05-16 11:23:58 +0200

    Aggiungere i file di progetto.

---
+++ net-il-mio-fotoalbum/Models/Photo.cs
@@ -0,0 +1,17 @@
+﻿namespace net_il_mio_fotoalbum.Models
+{
+    public class Photo
+    {
+        public long Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public byte[] Image { get; set; }
+
+        public bool Visible { get; set; }
+
+        public List<Category>? Categories { get; set; }
+    }
+}
