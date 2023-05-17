50bfb0098508b8f0643fdc98f98cc6d4328f6ddc
Andrea Bonaventura <andreabonaventura802@gmail.com>
2023-05-17 12:55:43 +0200

    Fixato create e edit

--- net-il-mio-fotoalbum/Controllers/PhotoController.cs
+++ net-il-mio-fotoalbum/Controllers/PhotoController.cs
@@ -13,11 +13,12 @@ namespace net_il_mio_fotoalbum.Controllers
         [HttpGet]
         public IActionResult Index()
         {
-            using(PhotoContext db = new PhotoContext())
+            using (PhotoContext db = new PhotoContext())
             {
-                List <Photo> photos = db.Photos.ToList();
+                List<Photo> photos = db.Photos.Where(ph => ph.Visibility == true).ToList();
 
                 List<string> imagesData = new List<string>();
+
                 foreach (Photo photo in photos)
                 {
                     imagesData.Add(Convert.ToBase64String(photo.Image));
@@ -36,7 +37,7 @@ namespace net_il_mio_fotoalbum.Controllers
         [HttpGet]
         public IActionResult Details(long id)
         {
-            using(PhotoContext db = new PhotoContext())
+            using (PhotoContext db = new PhotoContext())
             {
                 Photo photo = db.Photos.Where(ph => ph.Id == id).Include(ph => ph.Categories).FirstOrDefault();
 
@@ -83,97 +84,147 @@ namespace net_il_mio_fotoalbum.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(PhotoFormModel model)
         {
-            if (ModelState.IsValid)
+            if (!ModelState.IsValid)
             {
-                // Gestisci l'immagine solo se è stata fornita
-                if (model.ImageFile != null && model.ImageFile.Length > 0)
+                using (PhotoContext db = new PhotoContext())
                 {
-                    using (PhotoContext db = new PhotoContext())
+                    List<Category> categories = db.Categories.ToList();
+
+                    List<SelectListItem> listCategories = new List<SelectListItem>();
+
+                    foreach (Category category in categories)
                     {
-                        // Leggi i dati dell'immagine come array di byte
-                        byte[] imageData = null;
-                        using (var binaryReader = new BinaryReader(model.ImageFile.OpenReadStream()))
+                        listCategories.Add(new SelectListItem
                         {
-                            imageData = binaryReader.ReadBytes((int)model.ImageFile.Length);
-                        }
+                            Text = category.Name,
+                            Value = category.Id.ToString()
+                        });
+                    }
 
-                        List<Category> categories = new List<Category>();
-                        foreach (var categoryid in model.SelectedCategories)
-                        {
-                            int intcategoryid = int.Parse(categoryid);
-                            var category = db.Categories.Where(c => c.Id == intcategoryid).FirstOrDefault();
-                            categories.Add(category);
-                        }
+                    Photo photo = new Photo();
+                    photo.Visibility = true;
 
-
-                        // Crea un nuovo oggetto Photo e assegna i valori
-                        Photo photo = new Photo
-                        {
-                            Title = model.Photo.Title,
-                            Description = model.Photo.Description,
-                            Image = imageData,
-                            Visibility = model.Photo.Visibility,
-                            Categories = categories
-                            // Assegna altre proprietà necessarie
-                        };
-
-                        // Salva l'oggetto Photo nel database
-
-                            db.Photos.Add(photo);
-                            db.SaveChanges();
-                        }
-
-                        return RedirectToAction("Index");
+                    model.Categories = listCategories;
+                    model.Photo = photo;
+                    return View(model);
                 }
             }
 
-            // Se il modello non è valido, ritorna la vista "Create" con il modello
-            return View(model);
-        }
-
-
-        [HttpGet]
-        public IActionResult Update(long id)
-        {
             using (PhotoContext db = new PhotoContext())
             {
-                Photo photo = db.Photos.FirstOrDefault(ph => ph.Id == id);
 
-                if (photo == null)
+
+                if (model.ImageFile == null)
+                    return View("Error", "Immagine non selezionata");
+
+                byte[] imageData = null;
+                using (var binaryReader = new BinaryReader(model.ImageFile.OpenReadStream()))
                 {
-                    return NotFound();
+                    imageData = binaryReader.ReadBytes((int)model.ImageFile.Length);
                 }
 
-                PhotoFormModel model = new PhotoFormModel();
-                List<Category> categories = db.Categories.ToList();
 
-                List<SelectListItem> listCategories = new List<SelectListItem>();
+                if (model.SelectedCategories == null)
+                    return View("Error", "Categorie non selezionate");
 
-                photo.Categories = new List<Category>();
-                foreach (Category category in categories)
+                List<Category> categories = new List<Category>();
+                foreach (var categoryid in model.SelectedCategories)
                 {
-                    listCategories.Add(new SelectListItem
-                    {
-                        Text = category.Name,
-                        Value = category.Id.ToString(),
-                        Selected = photo.Categories.Any(c => c.Id == category.Id)
-                    });
+                    int intcategoryid = int.Parse(categoryid);
+                    var category = db.Categories.Where(c => c.Id == intcategoryid).FirstOrDefault();
+                    categories.Add(category);
                 }
 
-                model.Categories = listCategories;
-                model.Photo = photo;
-                model.Image = Convert.ToBase64String(photo.Image);
+                Photo photo = new Photo()
+                {
+                    Title = model.Photo.Title,
+                    Description = model.Photo.Description,
+                    Visibility = model.Photo.Visibility,
+                    Image = imageData,
+                    Categories = categories
+                };
 
-                return View(model);
+                db.Photos.Add(photo);
+                db.SaveChanges();
+                return RedirectToAction("Index");
             }
         }
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Update(long id, PhotoFormModel model)
-        {
-            if (ModelState.IsValid)
+
+            [HttpGet]
+            public IActionResult Update(long id)
             {
+                using (PhotoContext db = new PhotoContext())
+                {
+                    Photo photo = db.Photos.FirstOrDefault(ph => ph.Id == id);
+
+                    if (photo == null)
+                    {
+                        return NotFound();
+                    }
+
+                    PhotoFormModel model = new PhotoFormModel();
+                    List<Category> categories = db.Categories.ToList();
+
+                    List<SelectListItem> listCategories = new List<SelectListItem>();
+
+                    photo.Categories = new List<Category>();
+                    foreach (Category category in categories)
+                    {
+                        listCategories.Add(new SelectListItem
+                        {
+                            Text = category.Name,
+                            Value = category.Id.ToString(),
+                            Selected = photo.Categories.Any(c => c.Id == category.Id)
+                        });
+                    }
+
+                    model.Categories = listCategories;
+                    model.Photo = photo;
+                    model.Image = Convert.ToBase64String(photo.Image);
+
+                    return View(model);
+                }
+            }
+
+            [HttpPost]
+            [ValidateAntiForgeryToken]
+            public IActionResult Update(long id, PhotoFormModel model)
+            {
+            if (!ModelState.IsValid)
+            {
+                using (PhotoContext db = new PhotoContext())
+                {
+                    Photo photo = db.Photos.FirstOrDefault(ph => ph.Id == id);
+
+                    if (photo == null)
+                    {
+                        return NotFound();
+                    }
+
+                    List<Category> categories = db.Categories.ToList();
+
+                    List<SelectListItem> listCategories = new List<SelectListItem>();
+
+                    photo.Categories = new List<Category>();
+                    foreach (Category category in categories)
+                    {
+                        listCategories.Add(new SelectListItem
+                        {
+                            Text = category.Name,
+                            Value = category.Id.ToString(),
+                            Selected = photo.Categories.Any(c => c.Id == category.Id)
+                        });
+                    }
+
+                    model.Categories = listCategories;
+                    model.Photo = photo;
+                    model.Image = Convert.ToBase64String(photo.Image);
+
+                    return View(model);
+                }
+            }
+
                 using (PhotoContext db = new PhotoContext())
                 {
                     Photo photo = db.Photos
@@ -185,7 +236,6 @@ namespace net_il_mio_fotoalbum.Controllers
                         return NotFound();
                     }
 
-                    // Leggi i dati dell'immagine come array di byte
                     if (model.ImageFile != null && model.ImageFile.Length > 0)
                     {
                         byte[] imageData = null;
@@ -203,10 +253,8 @@ namespace net_il_mio_fotoalbum.Controllers
 
                     if (model.SelectedCategories != null && model.SelectedCategories.Any())
                     {
-                        // Rimuovi tutte le categorie esistenti per questa foto
                         photo.Categories.Clear();
 
-                        // Aggiungi le categorie selezionate
                         foreach (var categoryId in model.SelectedCategories)
                         {
                             int intCategoryId = int.Parse(categoryId);
@@ -222,27 +270,26 @@ namespace net_il_mio_fotoalbum.Controllers
 
                     return RedirectToAction("Index");
                 }
+
+
             }
 
-            return View(model);
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Delete(long id)
-        {
-            using(PhotoContext db = new PhotoContext())
+            [HttpPost]
+            [ValidateAntiForgeryToken]
+            public IActionResult Delete(long id)
             {
-                Photo photo = db.Photos.Where(ph => ph.Id == id).FirstOrDefault();
-                if(photo != null)
+                using (PhotoContext db = new PhotoContext())
                 {
-                    db.Photos.Remove(photo);
-                    db.SaveChanges();
+                    Photo photo = db.Photos.Where(ph => ph.Id == id).FirstOrDefault();
+                    if (photo != null)
+                    {
+                        db.Photos.Remove(photo);
+                        db.SaveChanges();
+                        return RedirectToAction("Index");
+                    }
+
                     return RedirectToAction("Index");
                 }
-
-                return RedirectToAction("Index");
             }
         }
     }
-}
